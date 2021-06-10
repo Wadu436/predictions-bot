7d1f52b0b9afc20b6187c2c1df1f3c18e71f79b6
Warre Dujardin <warre.dujardin@gmail.com>
2021-06-10 02:47:18 +0200

    get extra info from match schedule

--- src/aiomediawiki/tables/matchschedule.py
+++ src/aiomediawiki/tables/matchschedule.py
@@ -13,6 +13,8 @@ class MatchScheduleRow:
         "DateTime_UTC",
         "BestOf",
         "MatchId",
+        "Tab",
+        "N_MatchInTab",
     }
     table = "MatchSchedule"
 
@@ -20,23 +22,27 @@ class MatchScheduleRow:
     team2: str
     winner: int
 
-    team1_score: str
-    team2_score: str
+    team1_score: int
+    team2_score: int
     best_of: int
 
     start: datetime
 
     match_id: str
+    tab: str
+    n_matchintab: int
 
     @classmethod
     def from_row(cls, row):
         return MatchScheduleRow(
             team1=row["Team1"],
             team2=row["Team2"],
-            winner=row["Winner"],
-            team1_score=row["Team1Score"],
-            team2_score=row["Team2Score"],
-            best_of=row["BestOf"],
+            winner=None if row["Winner"] == "" else int(row["Winner"]),
+            team1_score=None if row["Team1Score"] == "" else int(row["Team1Score"]),
+            team2_score=None if row["Team2Score"] == "" else int(row["Team2Score"]),
+            best_of=int(row["BestOf"]),
             start=datetime.strptime(row["DateTime UTC"], "%Y-%m-%d %H:%M:%S"),
             match_id=row["MatchId"],
+            tab=row["Tab"],
+            n_matchintab=int(row["N MatchInTab"]),
         )
