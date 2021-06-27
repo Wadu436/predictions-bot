ace6c4664e71fe3a4cefd11358b5096ca1783207
Warre Dujardin <warre.dujardin@gmail.com>
2021-06-28 00:35:17 +0200

    delete the old database scripts

--- src/database-scripts/emoji_str_to_int.sql
+++
@@ -1,3 +0,0 @@
---SELECT  FROM teams;
---UPDATE teams SET emoji=(regexp_matches(emoji, '<a?:.+:(\d+)>'))[1];
-ALTER TABLE teams ALTER COLUMN emoji TYPE bigint USING (substring (emoji FROM '<a?:.+:(\d+)>'))::bigint;
