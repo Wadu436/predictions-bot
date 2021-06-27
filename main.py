4d2155f8d7ddea2897069d86ce1a80c676f851ed
Warre Dujardin <warre.dujardin@gmail.com>
2021-06-27 15:29:07 +0200

    let config specify logging level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 
+import config
 import src.bot
 
 persistPath = Path.cwd() / "persistent"
@@ -9,7 +10,7 @@ persistPath.mkdir(parents=True, exist_ok=True)
 logPath = persistPath / "bot.log"
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=getattr(config, "logging_level", logging.INFO),
     style="{",
     format="{asctime:19s} [{levelname:8s}] {message}",
     datefmt="%Y-%m-%d %H:%M:%S",
